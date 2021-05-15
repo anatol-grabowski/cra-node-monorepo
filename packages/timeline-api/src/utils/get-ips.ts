e0fdd4a6e95fc1785599429c347425fa2bd33fb6
tot <grabantot@gmail.com>
2021-05-15 19:09:52 +0300

    updated configs, added .tool-versions

---
+++ packages/timeline-api/src/utils/get-ips.ts
@@ -0,0 +1,13 @@
+import os, { NetworkInterfaceInfo } from 'os'
+
+export function getIps(): [ifaceName: string, ip: string][] {
+  const res: [ifaceName: string, ip: string][] = []
+  const ifaces = os.networkInterfaces()
+  for (const [ifaceName, nets] of Object.entries(ifaces)) {
+    for (const net of nets as NetworkInterfaceInfo[]) {
+      if (net.family === 'IPv6') continue
+      res.push([ifaceName, net.address])
+    }
+  }
+  return res
+}
