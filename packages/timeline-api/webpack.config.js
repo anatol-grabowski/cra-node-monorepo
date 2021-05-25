f336d4c1626459004f6865b1a6122d209dc7ac27
tot <grabantot@gmail.com>
2021-05-25 23:18:27 +0300

    disabled some webpack warnings

--- packages/timeline-api/webpack.config.js
+++ packages/timeline-api/webpack.config.js
@@ -1,27 +1,60 @@
 const path = require('path')
 const TsconfigPathsPlugin = require('tsconfig-paths-webpack-plugin')
 
-module.exports = {
-  entry: './src/main.ts',
-  output: {
-    filename: 'main.js',
-    path: path.resolve(__dirname, 'dist'),
-  },
-  target: 'node',
-  resolve: {
-    extensions: ['.tsx', '.ts', '.js'],
-    plugins: [new TsconfigPathsPlugin({})],
-  },
-  module: {
-    rules: [
+module.exports = function getConfig(env, { mode }) {
+  const config = {
+    entry: './src/main.ts',
+    output: {
+      filename: 'main.js',
+      path: path.resolve(__dirname, 'dist'),
+    },
+    target: 'node',
+    resolve: {
+      extensions: ['.tsx', '.ts', '.js'],
+      plugins: [new TsconfigPathsPlugin({})],
+    },
+    module: {
+      rules: [
+        {
+          test: /\.tsx?$/,
+          use: 'ts-loader',
+        },
+      ],
+    },
+    devtool: 'inline-source-map',
+    optimization: {
+      minimize: false,
+    },
+    stats: {
+      hash: false,
+      builtAt: false,
+      assets: false,
+      entrypoints: true,
+      chunks: false,
+      chunkOrigins: false,
+      modules: false,
+      timings: true,
+      colors: true,
+      version: false,
+      modules: false,
+      reasons: false,
+      children: false,
+      source: false,
+      errorDetails: false,
+      publicPath: false,
+      warningsCount: true,
+      warnings: true,
+    },
+  }
+  if (mode !== 'production') {
+    config.ignoreWarnings = [
+      { module: /\/ws\/lib\//, message: /Can't resolve 'utf-8-validate'/ },
+      { module: /\/ws\/lib\//, message: /Can't resolve 'bufferutil'/ },
       {
-        test: /\.tsx?$/,
-        use: 'ts-loader',
+        module: /\/any-promise\//,
+        message: /Critical dependency: the request of a dependency is an expression/,
       },
-    ],
-  },
-  devtool: 'inline-source-map',
-  optimization: {
-    minimize: false,
-  },
+    ]
+  }
+  return config
 }
