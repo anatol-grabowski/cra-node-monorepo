a0d9bb3c4c8d6be055a1fe778bf915891f999ccc
tot <grabantot@gmail.com>
2022-04-07 01:42:51 +0200

    updated config-overrides

--- packages/web/config-overrides.js
+++ packages/web/config-overrides.js
@@ -1,6 +1,12 @@
-const { alias, configPaths } = require('react-app-rewire-alias')
+const path = require('path')
+const ModuleScopePlugin = require('react-dev-utils/ModuleScopePlugin')
 
 module.exports = function override(config) {
-  alias(configPaths('../../tsconfig.base.json'))(config)
+  config.resolve.plugins = config.resolve.plugins.filter(
+    (plugin) => !(plugin instanceof ModuleScopePlugin),
+  )
+  config.resolve = config.resolve || {}
+  config.resolve.alias = config.resolve.alias || {}
+  config.resolve.alias['@common'] = path.resolve(__dirname, '../common/src')
   return config
 }
