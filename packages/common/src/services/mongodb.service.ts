9c0b19c804839ff8240edbe96d6ad193709be85c
tot <grabantot@gmail.com>
2022-04-09 16:19:14 +0200

    upd req svc

--- packages/common/src/services/mongodb.service.ts
+++ packages/common/src/services/mongodb.service.ts
@@ -1,19 +1,15 @@
 import { MongoClient, Db } from 'mongodb'
 
+export { Collection } from 'mongodb'
+
 export class MongodbService {
-  private url: string | null
-  private client: MongoClient | null
-  db: Db | null
+  protected client: MongoClient | null = null
+  db: Db | null = null
 
-  constructor() {
-    this.url = null
-    this.client = null
-    this.db = null
-  }
+  constructor(protected url: string) {}
 
-  async connect(url: string) {
-    this.url = url
-    this.client = await MongoClient.connect(url, { useNewUrlParser: true })
+  async connect() {
+    this.client = await MongoClient.connect(this.url, { useNewUrlParser: true })
     this.db = this.client.db()
   }
 }
