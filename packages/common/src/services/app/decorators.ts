009217a0a47855a9bd9ed169d9c399bf9b1fd05e
tot <grabantot@gmail.com>
2022-04-24 17:11:51 +0200

    more decorators

--- packages/common/src/services/app/decorators.ts
+++ packages/common/src/services/app/decorators.ts
@@ -31,3 +31,39 @@ export function Get(route: string) {
     target[routerSymbol].get(route, target[name])
   }
 }
+
+export function Post(route: string) {
+  return function (target, name) {
+    if (target[routerSymbol] == null) {
+      target[routerSymbol] = new Router()
+    }
+    target[routerSymbol].post(route, target[name])
+  }
+}
+
+export function Put(route: string) {
+  return function (target, name) {
+    if (target[routerSymbol] == null) {
+      target[routerSymbol] = new Router()
+    }
+    target[routerSymbol].put(route, target[name])
+  }
+}
+
+export function Patch(route: string) {
+  return function (target, name) {
+    if (target[routerSymbol] == null) {
+      target[routerSymbol] = new Router()
+    }
+    target[routerSymbol].patch(route, target[name])
+  }
+}
+
+export function Delete(route: string) {
+  return function (target, name) {
+    if (target[routerSymbol] == null) {
+      target[routerSymbol] = new Router()
+    }
+    target[routerSymbol].delete(route, target[name])
+  }
+}
